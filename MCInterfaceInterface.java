647707d780c3ddaa95cd5e816ef0e5225c50c61f
YaLian2003 <54329751+YaLian2003@users.noreply.github.com>
2023-12-29 00:14:57 -0800

    Create MCInterfaceInterface.java

---
+++ MCInterfaceInterface.java
@@ -0,0 +1,7 @@
+public interface MCInterface {
+    String encode(String s);
+    String decode(String s);
+    void inorderTraverse();
+    void preorderTraverse();
+    void postorderTraverse();
+}
